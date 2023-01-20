8514c6d52c26e4252c7bdbc5ea6452d85037f96c
Ahmad khatab <dev.ahmad.khattab@gmail.com>
2023-01-20 10:07:03 +0300

    add tests

---
+++ __tests__/hellotext_test.js
@@ -0,0 +1,87 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Hellotext from "../lib/hellotext";
+
+const getCookieValue = name => document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop()
+
+describe("when trying to call methods before initializing the class", () => {
+  it("raises an error when Hellotext.session is called",  () => {
+    expect(() => Hellotext.session).toThrowError()
+  });
+
+  it("raises an error when Hellotext.track is called",  () => {
+    expect(Hellotext.track("page.viewed")).rejects.toThrowError()
+  });
+})
+
+describe("when the class is initialized successfully", () => {
+  const business_id = "xy76ks"
+
+  describe("when hello_session is present in the query params", () => {
+    beforeAll(() => {
+      const windowMock = {
+        location: { search: "?hello_session=session" },
+      }
+
+      jest.spyOn(global, 'window', 'get').mockImplementation(() => windowMock)
+      Hellotext.initialize(business_id)
+    })
+
+    it("sets the cookie as value of the query parameter", () => {
+      expect(getCookieValue("hello_session")).toEqual("session")
+    });
+
+    it("returns the value when Hellotext.session is called", () => {
+      expect(Hellotext.session).toEqual("session")
+    });
+
+    describe("when tracking events", () => {
+      it("success attribute is true when response from the server is received successfully", async () => {
+        global.fetch = jest.fn().mockResolvedValue({
+          json: jest.fn().mockResolvedValue({received: "success"}),
+          status: 200
+        })
+
+        const response = await Hellotext.track("page.viewed")
+
+        expect(response.success).toEqual(true)
+      });
+
+      it("success attribute is false when response from the server is rejected", async () => {
+        global.fetch = jest.fn().mockResolvedValue({
+          json: jest.fn().mockResolvedValue({}),
+          status: 422
+        })
+
+        const response = await Hellotext.track("page.viewed")
+
+        expect(response.success).toEqual(false)
+      });
+    });
+  });
+
+  describe("when hello_session is not present in the query params", () => {
+    const business_id = "xy76ks"
+
+    beforeAll(() => {
+      const windowMock = {location: { search: "" },}
+      jest.spyOn(global, 'window', 'get').mockImplementation(() => windowMock)
+    })
+
+    it("mints a new session token and sets the cookie as well", () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue({id: "generated_token"}),
+        status: 200
+      })
+
+      Hellotext.initialize(business_id)
+
+      setTimeout(() => {
+        expect(getCookieValue("hello_session")).toEqual("generated_token")
+        expect(Hellotext.session).toEqual("generated_token")
+      }, 1000)
+    });
+  });
+});
