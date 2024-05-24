9fad062a43560a0798b01fe7644e7e23d65db1ab
rockwellll <dev.ahmad.khattab@gmail.com>
2024-05-24 14:32:13 +0300

    ad test for business model

---
+++ __tests__/models/business_test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+import { Business } from '../../src/models/business'
+
+beforeEach(() => {
+  global.fetch = jest.fn().mockResolvedValue({ json: jest.fn().mockResolvedValue({}) })
+})
+
+describe('enabledWhitelist', () => {
+  it('is true when the whitelist is an array', () => {
+    const business = new Business('123')
+    business.data = { whitelist: ['www.example.com'] }
+
+    expect(business.enabledWhitelist).toEqual(true)
+  })
+
+  it('is false when the whitelist is a string', () => {
+    const business = new Business('123')
+    business.data = { whitelist: 'disabled' }
+
+    expect(business.enabledWhitelist).toEqual(false)
+  })
+})
