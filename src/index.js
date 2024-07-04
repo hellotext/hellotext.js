016f9dc6d3e904d4c411e19c27ea187bb2493463
rockwellll <dev.ahmad.khattab@gmail.com>
2024-07-04 16:11:14 +0300

    WIP

--- src/index.js
+++ src/index.js
@@ -10,26 +10,4 @@ application.register('hellotext--otp', OTPController)
 
 // import '../styles/index.css'
 
-addEventListener('DOMContentLoaded', () => {
-  console.log('loadedd')
-  Hellotext.forms.collect()
-})
-
-addEventListener('readystatechange', () => {
-
-})
-
-document.onreadystatechange = () => {
-  console.log(document.readyState)
-
-  if(document.readyState === 'complete') {
-    console.log('loadedd 2')
-    Hellotext.forms.collect()
-  }
-}
-
-addEventListener('load', () => {
-  console.log('loadedd 3')
-  Hellotext.forms.collect()
-})
 export default Hellotext
