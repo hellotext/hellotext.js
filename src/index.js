62a311af4a24e527730a47930709581eb02b51a0
rockwellll <dev.ahmad.khattab@gmail.com>
2024-07-04 16:15:26 +0300

    update code. CI SKIP

--- src/index.js
+++ src/index.js
@@ -10,4 +10,14 @@ application.register('hellotext--otp', OTPController)
 
 // import '../styles/index.css'
 
+const observer = new MutationObserver((mutationsList) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+      console.log(document.querySelectorAll('[data-hello-form]'))
+    }
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
 export default Hellotext
