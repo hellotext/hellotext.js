d8252f3c680986f8d656e8f174bfc92c2b33abfa
rockwellll <dev.ahmad.khattab@gmail.com>
2024-09-20 11:48:00 +0300

    update code

--- src/core/configuration.js
+++ src/core/configuration.js
@@ -1,13 +1,22 @@
+import { Application } from '@hotwired/stimulus'
+import FormController from '../controllers/form_controller'
+
 class Configuration {
   static apiRoot = 'https://api.hellotext.com/v1'
   static autoGenerateSession = true
   static autoMountForms = true
+  static useStimulus = true
 
   static assign(props) {
     if(props) {
       Object.entries(props).forEach(([key, value]) => {
         this[key] = value
       })
+    }
+
+    if(this.useStimulus) {
+      const application = Application.start()
+      application.register('hellotext--form', FormController)
     }
 
     return this
