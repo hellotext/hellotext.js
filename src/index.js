0c790dd57f10290d579276aeb9dee4d22d7800d0
rockwellll <dev.ahmad.khattab@gmail.com>
2024-09-20 11:51:36 +0300

    update code again

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import { Application } from '@hotwired/stimulus'
+import Hellotext from './hellotext'
+
+import FormController from './controllers/form_controller'
+
+const application = Application.start()
+application.register('hellotext--form', FormController)
+
 import '../styles/index.css'
-export default from './hellotext'
+
+window.Hellotext = Hellotext
+
+export default Hellotext
