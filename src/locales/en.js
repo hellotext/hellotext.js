3d66238e4adc33ae3bd9af01c2f2f22a98541b68
rockwellll <dev.ahmad.khattab@gmail.com>
2024-10-14 10:27:29 +0300

    add new localization

--- src/locales/en.js
+++ src/locales/en.js
@@ -5,5 +5,10 @@ export default {
   errors: {
     parameter_not_unique: 'This value is taken.',
     blank: 'This field is required.'
+  },
+  forms: {
+    phone: 'Click the link sent via SMS to verify your submission.',
+    email: 'Click the link sent via email to verify your submission.',
+    phone_and_email: 'Click the links sent via SMS and email to verify your submission.',
   }
 }
