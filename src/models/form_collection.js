0712dc450c5d49fea841ce7090c2d015881f7211
rockwellll <dev.ahmad.khattab@gmail.com>
2024-10-14 10:54:49 +0300

    add logs

--- src/models/form_collection.js
+++ src/models/form_collection.js
@@ -30,6 +30,9 @@ class FormCollection {
 
     const forms = Array.from(document.querySelectorAll('[data-hello-form]'))
 
+    console.log(Configuration.forms.autoMount)
+    console.log(Configuration)
+
     if (forms && Configuration.forms.autoMount) {
       this.collect()
     }
