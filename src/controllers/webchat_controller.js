899b872739f0eaa4974aa5b41e4bc1fba8cadf5d
rockwellll <dev.ahmad.khattab@gmail.com>
2024-12-29 10:29:05 +0300

    add controller for webchat

---
+++ src/controllers/webchat_controller.js
@@ -0,0 +1,86 @@
+import { Controller } from '@hotwired/stimulus'
+import { computePosition, autoUpdate, shift, flip, offset } from '@floating-ui/dom'
+
+export default class extends Controller {
+  static values = {
+    placement: { type: String, default: "bottom-end" },
+    open: { type: Boolean, default: false },
+    autoPlacement: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false },
+  }
+
+  static targets = [
+    'trigger',
+    'popover',
+  ]
+
+  connect() {
+    this.floatingUICleanup = autoUpdate(this.triggerTarget, this.popoverTarget, () => {
+      computePosition(this.triggerTarget, this.popoverTarget, {
+        placement: this.placementValue,
+        middleware: this.middlewares,
+      }).then(({x, y}) => {
+        const newStyle = {
+          left: `${x}px`,
+          top: `${y}px`
+        }
+
+        Object.assign(this.popoverTarget.style, newStyle);
+      });
+    })
+
+    super.connect()
+  }
+
+  disconnect() {
+    this.floatingUICleanup()
+    super.disconnect()
+  }
+
+  show() {
+    this.openValue = true
+  }
+
+  hide() {
+    this.openValue = false
+  }
+
+  toggle() {
+    this.openValue = !this.openValue
+  }
+
+  onClickOutside(event) {
+    if (event.target.nodeType && this.element.contains(event.target) === false) {
+      this.openValue = false
+      setTimeout(() => this.dispatch("aborted"), 400)
+    }
+  }
+
+  openValueChanged() {
+    if(this.disabledValue) return
+
+    this.dispatch("toggle", {
+      detail: this.openValue
+    })
+
+    if(this.openValue) {
+      this.popoverTarget.showPopover()
+      this.popoverTarget.setAttribute("aria-expanded", "true")
+
+      this.dispatch("opened")
+    } else {
+      this.popoverTarget.hidePopover()
+      this.popoverTarget.removeAttribute("aria-expanded")
+
+      this.dispatch("hidden")
+    }
+  }
+
+  get middlewares() {
+    return [
+      offset(5),
+      shift({ padding: 24 }),
+      flip(),
+    ]
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import { Application } from '@hotwired/stimulus'
 import Hellotext from './hellotext'
 
 import FormController from './controllers/form_controller'
+import WebChatController from './controllers/webchat_controller'
 
 const application = Application.start()
+
 application.register('hellotext--form', FormController)
+application.register('hellotext--webchat', WebChatController)
 
 import '../styles/index.css'
 
