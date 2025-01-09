e8ab4062acda59d39d0de26ee8fadf0f692adc85
rockwellll <dev.ahmad.khattab@gmail.com>
2025-01-09 07:44:43 +0300

    update tests

--- __tests__/core/configuration/webchat_test.js
+++ __tests__/core/configuration/webchat_test.js
@@ -1,109 +1,109 @@
-import { WebChat } from '../../../src/core/configuration/web_chat'
+import { Webchat } from '../../../src/core/configuration/webchat'
 
-describe('WebChat', () => {
+describe('Webchat', () => {
   describe('behaviour', () => {
     it('is POPOVER by default', () => {
-      expect(WebChat.behaviour).toEqual('popover')
+      expect(Webchat.behaviour).toEqual('popover')
     });
 
     it('can be set to modal', () => {
-      WebChat.behaviour = 'modal'
-      expect(WebChat.behaviour).toEqual('modal')
+      Webchat.behaviour = 'modal'
+      expect(Webchat.behaviour).toEqual('modal')
     });
 
     it('throws an exception when an invalid value is supplied', () => {
       expect(() => {
-        WebChat.behaviour = 'invalid'
+        Webchat.behaviour = 'invalid'
       }).toThrowError('Invalid behaviour value: invalid')
     });
   })
 
   describe('container', () => {
     it('is body by default', () => {
-      expect(WebChat.container).toEqual('body')
+      expect(Webchat.container).toEqual('body')
     });
 
     it('can be set to any other value', () => {
-      WebChat.container = 'html'
-      expect(WebChat.container).toEqual('html')
+      Webchat.container = 'html'
+      expect(Webchat.container).toEqual('html')
     });
   })
 
   describe('placement', () => {
     it('is bottom-right by default', () => {
-      expect(WebChat.placement).toEqual('bottom-right')
+      expect(Webchat.placement).toEqual('bottom-right')
     });
 
     it('can be set to any other value', () => {
-      WebChat.placement = 'top-left'
-      expect(WebChat.placement).toEqual('top-left')
+      Webchat.placement = 'top-left'
+      expect(Webchat.placement).toEqual('top-left')
     });
 
     it('throws an exception when an invalid value is supplied', () => {
       expect(() => {
-        WebChat.placement = 'invalid'
+        Webchat.placement = 'invalid'
       }).toThrowError('Invalid placement value: invalid')
     });
   })
 
   describe('classes', () => {
     it('is undefined by default', () => {
-      expect(WebChat.classes).toEqual(undefined)
+      expect(Webchat.classes).toEqual(undefined)
     });
 
     describe('setting value to a String', () => {
       it('can be set to a string value', () => {
-        WebChat.classes = 'custom-class'
+        Webchat.classes = 'custom-class'
       });
 
       it('returns an array of the values', () => {
-        WebChat.classes = 'custom-class, another-class'
-        expect(WebChat.classes).toEqual(['custom-class', 'another-class'])
+        Webchat.classes = 'custom-class, another-class'
+        expect(Webchat.classes).toEqual(['custom-class', 'another-class'])
       });
     });
 
     it('can be set to an Array', () => {
-      WebChat.classes = ['custom-class']
-      expect(WebChat.classes).toEqual(['custom-class'])
+      Webchat.classes = ['custom-class']
+      expect(Webchat.classes).toEqual(['custom-class'])
     });
 
     it('throws an exception when an invalid value is supplied', () => {
       expect(() => {
-        WebChat.classes = { invalid: 'value' }
+        Webchat.classes = { invalid: 'value' }
       }).toThrowError('classes must be an array or a string')
     });
   })
 
   describe('triggerClasses', () => {
     it('is undefined by default', () => {
-      expect(WebChat.triggerClasses).toEqual(undefined)
+      expect(Webchat.triggerClasses).toEqual(undefined)
     });
 
     describe('setting value to a String', () => {
       it('can be set to a string value', () => {
-        WebChat.triggerClasses = 'custom-class'
+        Webchat.triggerClasses = 'custom-class'
       });
 
       it('returns an array of the values', () => {
-        WebChat.triggerClasses = 'custom-class, another-class'
-        expect(WebChat.triggerClasses).toEqual(['custom-class', 'another-class'])
+        Webchat.triggerClasses = 'custom-class, another-class'
+        expect(Webchat.triggerClasses).toEqual(['custom-class', 'another-class'])
       });
     });
 
     describe('when setting value to an Array', () => {
       it('can be set', () => {
-        WebChat.triggerClasses = ['custom-class']
+        Webchat.triggerClasses = ['custom-class']
       });
 
       it('returns the value', () => {
-        WebChat.triggerClasses = ['custom-class', 'another-class']
-        expect(WebChat.triggerClasses).toEqual(['custom-class', 'another-class'])
+        Webchat.triggerClasses = ['custom-class', 'another-class']
+        expect(Webchat.triggerClasses).toEqual(['custom-class', 'another-class'])
       });
     })
 
     it('throws an exception when an invalid value is supplied', () => {
       expect(() => {
-        WebChat.triggerClasses = { invalid: 'value' }
+        Webchat.triggerClasses = { invalid: 'value' }
       }).toThrowError('triggerClasses must be an array or a string')
     });
   })
