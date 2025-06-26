c58a8f0c9eac652a0088d0d4f22fdb2ba4c6718c
rockwellll <dev.ahmad.khattab@gmail.com>
2025-06-26 09:13:38 +0300

    ignore index.bundle.js for esm/commonJS builds

--- src/index.bundle.js
+++ src/index.bundle.js
@@ -1,7 +1,5 @@
-// Entry point for UMD bundle - includes CSS
 import '../styles/index.css'
 import './index.js'
 
-// Re-export the default
 import Hellotext from './index.js'
 export default Hellotext
