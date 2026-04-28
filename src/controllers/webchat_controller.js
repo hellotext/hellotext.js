8ad0dfcda5ac304c04d01450b76998bbb7522ad9
rockwellll <dev.ahmad.khattab@gmail.com>
2026-04-28 10:57:43 +0300

    setup teaser target

--- src/controllers/webchat_controller.js
+++ src/controllers/webchat_controller.js
@@ -84,7 +84,10 @@ export default class extends Controller {
     this.triggerTarget.classList.add(...WebchatConfiguration.triggerClasses)
 
     this.setupFloatingUI({ trigger: this.triggerTarget, popover: this.popoverTarget })
-    this.setupFloatingUI({ trigger: this.triggerTarget, popover: this.teaserTarget })
+
+    if (this.hasTeaserTarget) {
+      this.setupFloatingUI({ trigger: this.triggerTarget, popover: this.teaserTarget })
+    }
 
     this.webChatChannel.onMessage(this.onMessageReceived)
     this.webChatChannel.onTypingStart(this.onTypingStart)
